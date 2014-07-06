8d4135fd1c2a9edf9c98fdd8499cee842f1ca68b
Alvin <alvjou@gmail.com>
2014-07-06 16:31:16 -0500

    restructured script to just combine name, episode number, and extension. Added feature to manually rename a single video

--- AnimeFormatter.py
+++ AnimeFormatter.py
@@ -6,107 +6,113 @@ if len(sys.argv) != 2:
 	print "Usage: python " + os.path.basename(__file__) + " [directory with files]"
 	sys.exit()
 
+# get to directory
 DirToRename = sys.argv[1]
 os.chdir(DirToRename)
 AllFiles = os.listdir('.')
+
+# Print the files
 print "\nFiles in: " + DirToRename
 for curr in AllFiles:
 	print curr
-print "\nFile extension(s)? (default: .mkv)"
+
+# Get extension(s) from user
+print "\nFile extension(s)? (default: mkv) (Seperate with a space if multiple)"
 Extension = raw_input()
 if Extension == "":
 	Extension = "mkv"
-if not Extension.startswith('.'):
-	Extension = '.' + Extension
-print "\nCurrent Anime Name?"	
+Extensions = Extension.split(' ')
+for Ext in Extensions:
+	if not Ext.startswith('.'):
+		Ext = '.' + Ext
+
+# Get files ending in the appropriate extension(s)
+Files = list()
+for curr in AllFiles:
+	for Ext in Extensions:
+		if curr.endswith(Ext):
+			Files.append(curr)
+
+print "\nFiles with the extension(s): " + Extension
+for curr in Files:
+	print curr
+
+# Get names to use when parsing
+print "\nCurrent Anime Name?"
 Name = raw_input()
 print "\nNew Anime Name? (default: same as current)"
 New = raw_input()
 if New == "":
 	New = Name
-# print "Want the [720p] resolution tag there? Possible options include \"720p\" or \"1080p\" (default: no tag)"
-# Resolution = raw_input()
-# if not Resolution == "":
-# 	HasRes = True
+print "\n"
 
-# Navigate to the appropriate directory
-
-Files = list()
-
-# Get files ending in the appropriate extension
-for curr in AllFiles:
-	if curr.endswith(Extension):
-		Files.append(curr)
-
-# Generate new names
+# NewNames will store all the new names, so the user can confirm before changing the file names
 NewNames = list()
+OldNames = list()
+# Loop through all the files
 for i in range(len(Files)):
-	# Don't you just hate anime with '_' in the titles instead of spaces?
+	OldNames.append(Files[i])
 	NewName = Files[i]
-	NewExtension = Extension
-	BoxedRes = ""
-	HasRes = False
-	Options = ["[720p]", "[1080p]", "[h264 720p]", "(1280x720)", "(1280x720 h264)", "[1280x720][h264]", "[1280x720][x264]"]
-	for option in Options:
-		if option in NewName:
-			Appended = option
-			if "720" in option:
-				Appended = "[720p]"
-			elif "1080" in option:
-				Appended = "[1080p]"
-			NewExtension = Appended + NewExtension
-			HasRes = True
-			BoxedRes = option
 
-	# Replace those pesky underscores and dashes
-	NewName = NewName.replace('_',' ')
-	NewName = NewName.replace('-',' ')
+	# Get file extension
+	StringList = NewName.split('.')
+	Ext = StringList[len(StringList) - 1]
+	Ext = '.' + Ext
+
+	Appended = ""
+	# Add resolution
+	if "720" in NewName:
+		Appended = " [720p]" 
+	if "1080" in NewName:
+		Appended = " [1080p]"
 	
-	# Remove everything before Name
-	NewName = NewName[NewName.find(Name):]
-
-	# Replace everything in between the resolution and the extension 
-	if HasRes:
-		NewName = NewName[:NewName.find(BoxedRes)] + NewExtension
 	while "  " in NewName:
 		NewName = NewName.replace("  "," ")
 
-	# print NewName
-	StringList = NewName.split()
+	# Get episode number
+	EpisodeStart = NewName.find(Name) + len(Name) + 1
+	AfterName = NewName[EpisodeStart:]
+	Search = re.search("\d", AfterName)
+	if not Search:
+		print "Error parsing episode number"
+		sys.exit()
+	Start = AfterName[Search.start():]
+	Search = re.search("\D", Start)
+	EpisodeNum = Start[:Search.start()]
 
-	# Deals with stuff like Ep01, where the episode number is prepended with Ep, EP, or ep
-	# Apply new name and removing leading 0
-	if StringList[len(StringList) - 2].startswith("Ep") or StringList[len(StringList) - 2].startswith("EP") or StringList[len(StringList) - 2].startswith("ep"):
-		StringList[len(StringList) - 2] = StringList[len(StringList) - 2][2:]
-	if StringList[len(StringList) - 2].startswith('0'):
-		StringList[len(StringList) - 2] = StringList[len(StringList) - 2][1:]
-	if 'v' in StringList[len(StringList) - 2]:
-		StringList[len(StringList) - 2] = StringList[len(StringList) - 2][:StringList[len(StringList) - 2].find('v')]
+	# I dislike leading 0s
+	while EpisodeNum.startswith('0'):
+		EpisodeNum = EpisodeNum[1:]
 
-	# Apply the new extension in case the previous HasRes condition was not fulfilled
-	if not HasRes:
-		StringList[len(StringList) - 2] += NewExtension
-		StringList = StringList[:len(StringList) - 1]
-
-	# Rejoin the list
-	NewName = " ".join(StringList)
-	
-	# Replace the old name with the new one specified
-	NewName = NewName.replace(Name, New)
+	# Combine the name, the number, and the extension
+	NewName = New + " - " + EpisodeNum + Appended + Ext
 
 	# Add to our list of new names
 	NewNames.append(NewName)
 
 	# Print it
-	print (Files[i] + " ---> " + NewNames[i])
 
 # Check with user before altering files
 Responded = False
 while not Responded:
-	print "\nAre these new file names acceptable? (y/n)"
+	for i in range(len(Files)):
+		print ("%d) " + Files[i] + " ---> " + NewNames[i]) % i
+	print "\nAre these new file names acceptable? (y/n/s) (yes/no/some)"
 	Response = raw_input()
 	if (Response == 'y' or Response =='n'):
 		Responded = True
+	if Response == 's':
+		print "Which files do you not want to alter? Seperate the numbers with a space (1 4 20)"
+		ManuallyChange = raw_input()
+		# while ManuallyChange
+		ManList = ManuallyChange.split(' ')
+		for ToChange in ManList:
+			print "Enter new name for %d) %s ---> %s" % (int(ToChange), OldNames[int(ToChange)], NewNames[int(ToChange)])
+			
+			Result = raw_input()
+			if not Result == "":
+				NewNames[int(ToChange)] = Result
+
 
 if Response == 'n':
 	print "\nOk, bye!"
@@ -114,6 +120,9 @@ if Response == 'n':
 
 print "\nRenaming files..."
 # Rename all the files
+# print len(Files)
+# print len(NewNames)
 for i in range(len(Files)):
+	print (Files[i] + " ---> " + NewNames[i])
 	os.rename(Files[i], NewNames[i])
 
