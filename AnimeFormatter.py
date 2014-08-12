a79ac754406b89a6decd36ec230e10d7f2b77113
Alvin <alvjou@gmail.com>
2014-08-12 00:11:27 -0500

    Added fix for episode numbers not updating on manual changes

--- AnimeFormatter.py
+++ AnimeFormatter.py
@@ -1,6 +1,71 @@
 import sys, os, re
+def deepcopy(A):
+    rt = []
+    for elem in A:
+        if isinstance(elem,list):
+            rt.append(deepcopy(elem))
+        else:
+            rt.append(elem)
+    return rt
 
-# Ending format will be [AnimeName] [Episode #] [FileEnding]
+def GetNewNames(Files):
+	NewNames = list()
+	EpisodeNumbers = list()
+	NumDigits = 0
+
+	for i in range(len(Files)):
+		NewName = Files[i]
+		
+		# Get episode number
+		EpisodeStart = NewName.find(Name) + len(Name) + 1
+		AfterName = NewName[EpisodeStart:]
+		Search = re.search("\d", AfterName)
+		if not Search:
+			print "Error parsing episode number"
+			sys.exit()
+		Start = AfterName[Search.start():]
+		Search = re.search("\D", Start)
+		EpisodeNum = Start[:Search.start()]
+		EpisodeNumbers.append(EpisodeNum)	
+		if len(str(EpisodeNum)) > NumDigits:
+			NumDigits = len(str(EpisodeNum))
+			
+	for i in range(len(Files)):
+		NewName = Files[i]
+
+		# Get file extension
+		StringList = NewName.split('.')
+		Ext = StringList[len(StringList) - 1]
+		Ext = '.' + Ext
+
+		Appended = ""
+		# Add resolution
+		if "720" in NewName:
+			Appended = " [720p]" 
+		if "1080" in NewName:
+			Appended = " [1080p]"
+		if "480" in NewName:
+			Appended = " [480p]"
+
+		NewName = NewName.replace('-', ' ')
+		NewName = NewName.replace('_', ' ')
+		NewName = NewName.replace('.', ' ')
+		
+		while "  " in NewName:
+			NewName = NewName.replace("  "," ")
+
+		# Add Leading 0s
+		while len(str(EpisodeNumbers[i])) < NumDigits:
+			EpisodeNumbers[i] = "0" + EpisodeNumbers[i]
+		
+		# Combine the name, the number, and the extension
+		NewName = Name + " - " + EpisodeNumbers[i] + Appended + Ext
+		EpisodeNumbers.append(EpisodeNum)
+		# Add to our list of new names
+		NewNames.append(NewName)
+	return NewNames
+
+# Ending format will be [AnimeName] - [Episode #] [Resolution if applicable].extension
 # Make sure we have the correct format for the command line arguments
 if len(sys.argv) != 2:
 	print "Usage: python " + os.path.basename(__file__) + " [directory with files]"
@@ -49,112 +114,50 @@ while not Responded:
 # Get names to use when parsing
 DirList = os.getcwd().split('/')
 Name = DirList[len(DirList) - 1]
-	
-# print "\nCurrent Anime Name? (default: " + Name + ")"
-# ParsedName = raw_input()
-# if not ParsedName == "":
-# 	if not ParsedName == Name:
-# 		Response = ""
-# 		while not (Response == 'y' or Response == 'n'):
-# 			print "\nWould you like to rename the folder to: " + ParsedName + "? (y - yes/n - no)"
-# 			Response = raw_input()
-# 		if Response =='y':
-# 			os.chdir("..")
-# 			os.rename(Name, ParsedName)	
-# 			os.chdir(ParsedName)
-# 		Name = ParsedName
 
-# NewNames will store all the new names, so the user can confirm before changing the file names
-NewNames = list()
+# Save old names
 OldNames = list()
-EpisodeNumbers = list()
-# Loop through all the files
-
-NumDigits = 0
-
-for i in range(len(Files)):
-	NewName = Files[i]
-	
-	# Get episode number
-	EpisodeStart = NewName.find(Name) + len(Name) + 1
-	AfterName = NewName[EpisodeStart:]
-	Search = re.search("\d", AfterName)
-	if not Search:
-		print "Error parsing episode number"
-		sys.exit()
-	Start = AfterName[Search.start():]
-	Search = re.search("\D", Start)
-	EpisodeNum = Start[:Search.start()]
-	EpisodeNumbers.append(EpisodeNum)	
-	if len(str(EpisodeNum)) > NumDigits:
-		NumDigits = len(str(EpisodeNum))
-
 for i in range(len(Files)):
 	OldNames.append(Files[i])
-	NewName = Files[i]
 
-	# Get file extension
-	StringList = NewName.split('.')
-	Ext = StringList[len(StringList) - 1]
-	Ext = '.' + Ext
-
-	Appended = ""
-	# Add resolution
-	if "720" in NewName:
-		Appended = " [720p]" 
-	if "1080" in NewName:
-		Appended = " [1080p]"
-	if "480" in NewName:
-		Appended = " [480p]"
-
-	NewName = NewName.replace('-', ' ')
-	NewName = NewName.replace('_', ' ')
-	NewName = NewName.replace('.', ' ')
-	
-	while "  " in NewName:
-		NewName = NewName.replace("  "," ")
-
-	# Add Leading 0s
-	while len(str(EpisodeNumbers[i])) < NumDigits:
-		EpisodeNumbers[i] = "0" + EpisodeNumbers[i]
-	
-	# # Remove leading 0's
-	# while EpisodeNumbers[i][0] == '0':
-	# 	if len(EpisodeNumbers[i]) != 1:
-	# 		EpisodeNumbers[i] = EpisodeNumbers[i][1:]
-			
-	# Combine the name, the number, and the extension
-	NewName = Name + " - " + EpisodeNumbers[i] + Appended + Ext
-	EpisodeNumbers.append(EpisodeNum)
-	# Add to our list of new names
-	NewNames.append(NewName)
-
+# Get new names
+NewNames = GetNewNames(Files)
 
 # Check with user before altering files
 templog = ""
 Responded = False
+
+# We do altar Files, but not undo the changes after getting the new names
 while not Responded:
+	# reset log line in case we do not like these changes
 	templog = ""
 	for i in range(len(Files)):
 		line = ("%d) " + Files[i] + " ---> " + NewNames[i]) % i 
 		print line
 		templog += line + "\n"
-	print "\nAre these new file names acceptable? (y - yes/n - no/s - some)"
+	print "\nAre these new file names acceptable? (y - yes/n - no/file numbers to alter)"
 	Response = raw_input()
 	if (Response == 'y' or Response =='n'):
 		Responded = True
-	if Response == 's':
-		print "\nWhich files do you not want to alter? Seperate the numbers with a space (1 4 20)"
-		ManuallyChange = raw_input()
-		# while ManuallyChange
-		ManList = ManuallyChange.split(' ')
+	elif Response != "":
+		# Get new names manually
+		ManList = Response.split(' ')
+		TempNames = list()
 		for ToChange in ManList:
 			print "\nEnter new name for %d) %s ---> %s" % (int(ToChange), OldNames[int(ToChange)], NewNames[int(ToChange)])
 			print "Default will be unchanged\n"
 			Result = raw_input()
 			if not Result == "":
-				NewNames[int(ToChange)] = Result
+				TempNames.append(Result)
 
+		# Hide the ones we are manually changing before updating names. This makes sure the episode numbers are set correctly.
+		Files.pop(int(ToChange))
+		NewNames = GetNewNames(Files)
+
+		# Insert the manually changed values to NewNames and back to Files
+		for ToChange in ManList:
+			NewNames.insert(int(ToChange), TempNames.pop(0))
+		Files.insert(int(ToChange), OldNames[int(ToChange)])
 
 if Response == 'n':
 	print "\nOk, bye!"
@@ -163,9 +166,6 @@ if Response == 'n':
 log.write(templog + "\n")
 log.close()
 print "\nRenaming files..."
-# Rename all the files
-# print len(Files)
-# print len(NewNames)
 for i in range(len(Files)):
 	os.rename(Files[i], NewNames[i])
 
